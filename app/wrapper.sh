0f0b19adf23b296962aca7be19de64438b0710cf
warvbell <wardvanbelle@gmail.com>
2022-04-07 13:13:07 +0200

    removed sudo again

--- app/wrapper.sh
+++ app/wrapper.sh
@@ -7,7 +7,7 @@ cd Thesis_Automatic_Design_of_Cellular_Robots/app/
 julia initialize.jl 
 
 # install voxcraft and cmake
-sudo apt update;sudo apt install -y cmake libboost-all-dev;
+apt update;apt install -y cmake libboost-all-dev;
 rm voxcraft-sim -rf; git clone https://github.com/voxcraft/voxcraft-sim.git; cd voxcraft-sim/;
 mkdir build; cd build; cmake .. -DBOOST_INCLUDEDIR="/usr/include" -DBOOST_LIBRARYDIR="/usr/lib/x86_64-linux-gnu"; ln -s /usr/include/boost /opt/conda/bin/../x86_64-conda-linux-gnu/sysroot/usr/include/boost; make -j 10;
 
